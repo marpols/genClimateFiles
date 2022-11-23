3264a752fd5ff44f3c31cf8606be238291ecfc59
marpols <mar.pols@hotmail.co.uk>
2022-11-23 18:58:01 -0400

    Update fill_MD.R

--- fill_MD.R
+++ fill_MD.R
@@ -12,7 +12,7 @@ yrs <- c(2018,2021)
 #----------------------------------Funtions-------------------------------------
 read_climateFile <- function(stn){
   #read climateFile for weather station
-  weather_fp <- paste(getwd(), "/", stn,"/",stn,"_","climatefile","_",".csv", sep="")
+  weather_fp <- paste(getwd(), "/", stn,"/",stn,"_","climatefile.csv", sep="")
   weather_dat <- read.csv(weather_fp)
   return(weather_dat)
 }
@@ -24,4 +24,28 @@ read_NP_file <- function(stn) {
   return(NP_data)
 }
 
+get_missing <- function(climfile,NPfile,climcols,NPcols){
+ i <- 1
+ while (i <= length(climcols)){
+    c <- climcols[i]
+    d <- NPcols[i]
+    missing <- which(is.na(climfile[c]))
+    for (j in missing){
+      climfile[j,c] <- NPfile[j,d]
+    }
+    i <- i + 1
+ }
+ return(climfile)
+}
+
 #-------------------------------Code Begins ------------------------------------
+
+for(s in stn_list){
+  climateFile <- read_climateFile(s)
+  NPfile <- read_NP_file(s)
+  #add solar radiation data
+  climateFile$SolarRadiation <- NPfile$ALLSKY_SFC_SW_DWN
+  
+  write.csv(get_missing(climateFile,NPfile,c(6,7,8),c(9,10,11)), file = paste(s,"_climComp.csv"))
+  
+}
